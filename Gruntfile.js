7d67ab600e386e7d9ebe6bed833e33a605b61566
Martijn Russchen <martijn@factlink.com>
2014-02-21 19:54:09 +0100

    Basic layout

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'assets/stylesheets/style.css' : 'assets/stylesheets/base.scss'
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
+}
